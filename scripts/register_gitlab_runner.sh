141109e1055afd85316e183365ccf4b15228608b
Mikhail Zagoruiko <zagoruiko.mikhail@gmail.com>
2026-08-19 22:55:03 +1000

    [ALL] Working monorepo and initial bootstrap. Currently without k8s
    Working GitLab, semi-working GitLab Runner, working git commit/push via
    SSH. There's even Headlamp integration for K3d, but without proper CI/CD
    pipeline for compilation and upload of hello_world_service via GitLab
    Runner.

--- scripts/register_gitlab_runner.sh
+++
@@ -1,7 +0,0 @@
-docker compose exec gitlab-runner \
-  gitlab-runner register \
-    --non-interactive \
-    --url "http://gitlab" \
-    --token "$RUNNER_TOKEN" \
-    --executor "docker" \
-    --docker-image "alpine:latest"
