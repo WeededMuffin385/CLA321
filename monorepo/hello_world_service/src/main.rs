1f397bd8c4c9c10519917a29414a9c73d1b07b29
Mikhail Zagoruiko <zagoruiko.mikhail@gmail.com>
2026-08-22 03:11:16 +1000

    [ALL] Completely-completely working prototype

--- monorepo/hello_world_service/src/main.rs
+++ monorepo/hello_world_service/src/main.rs
@@ -1,3 +1,4 @@
+use tokio_util::sync::CancellationToken;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
 
@@ -9,8 +10,38 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    info!("Hello, World!");
+    
+    let shutdown = CancellationToken::new();
+    let shutdown_tmp = shutdown.clone();
+    let worker = tokio::spawn(async move {
+        let shutdown = shutdown_tmp;
+        
+        loop {
+            tokio::select! {
+                _ = shutdown.cancelled() => {
+                    info!("worker shutdown");
+                    break;
+                }
+                
+                _ = tokio::time::sleep(tokio::time::Duration::from_secs(5)) => {
+                    info!("tick");
+                }
+            }
+        }
+    });
 
+    info!("Hello, World!");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener, app::router()).await.unwrap()
+    axum::serve(listener, app::router()).with_graceful_shutdown({
+        let shutdown = shutdown.clone();
+        
+        async move {
+            tokio::signal::ctrl_c().await.unwrap();
+            info!("shutdown signal received");
+            shutdown.cancel();
+        }
+    }).await.unwrap();
+
+    worker.await.unwrap();
+    info!("shutdown complete");
 }
